66f90a7a12d942bb8ded69670ce88aa2d7202963
Afrochemist <yamaha366@aol.com>
2020-09-02 12:00:10 -0400

    parent.py

--- parent.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup 
-
-html = urlopen("http://pythonscraping.com/pages/page3.html")
-bsObj = BeautifulSoup(html)
-
-print(bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
